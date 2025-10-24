22c9749c4a4314446c1e57e41cd19f201527bedb
Luke Craig <lacraig3@gmail.com>
2025-10-24 10:30:46 -0400

    fixup portalcall arguments

--- src/portalcall.rs
+++ src/portalcall.rs
@@ -3,7 +3,7 @@ use libc::{syscall, SYS_sendto};
 #[cfg(target_pointer_width = "64")]
 const PORTAL_MAGIC: libc::c_ulong = 0xc1d1e1f1;
 #[cfg(not(target_pointer_width = "64"))]
-const PORTAL_MAGIC: libc::c_int = 0xc1d1e1f1;
+const PORTAL_MAGIC: libc::c_uint = 0xc1d1e1f1;
 
 #[inline]
 pub fn portal_call(user_magic: u64, argc: i32, args: &[u64]) -> i64 {
@@ -21,26 +21,31 @@ pub fn portal_call(user_magic: u64, argc: i32, args: &[u64]) -> i64 {
 }
 
 #[inline]
+#[allow(dead_code)]
 pub fn portal_call1(user_magic: u64, a1: u64) -> i64 {
     portal_call(user_magic, 1, &[a1])
 }
 
 #[inline]
+#[allow(dead_code)]
 pub fn portal_call2(user_magic: u64, a1: u64, a2: u64) -> i64 {
     portal_call(user_magic, 2, &[a1, a2])
 }
 
 #[inline]
+#[allow(dead_code)]
 pub fn portal_call3(user_magic: u64, a1: u64, a2: u64, a3: u64) -> i64 {
     portal_call(user_magic, 3, &[a1, a2, a3])
 }
 
 #[inline]
+#[allow(dead_code)]
 pub fn portal_call4(user_magic: u64, a1: u64, a2: u64, a3: u64, a4: u64) -> i64 {
     portal_call(user_magic, 4, &[a1, a2, a3, a4])
 }
 
 #[inline]
+#[allow(dead_code)]
 pub fn portal_call5(user_magic: u64, a1: u64, a2: u64, a3: u64, a4: u64, a5: u64) -> i64 {
     portal_call(user_magic, 5, &[a1, a2, a3, a4, a5])
 }
